8329f022d10bb3311d81c98f6cb94d110f6474e5
COMLABPC13-PC\ALLAN_Novalta <allannovalta@gmail.com>
2018-11-10 17:16:09 +0800

    CSM Question Poll startup

---
+++ Allannovalta.CSMQuestionPoll.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Allannovalta.CSMQuestionPoll.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
